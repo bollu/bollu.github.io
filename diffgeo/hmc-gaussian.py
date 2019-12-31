6d95cce5804bdcb0ac9b91dfaa37b5e641865b42
Siddharth Bhat <siddu.druid@gmail.com>
2020-01-01 02:02:07 +0530

    show HMC split

--- diffgeo/hmc-gaussian.py
+++ diffgeo/hmc-gaussian.py
@@ -34,17 +34,34 @@ def planet(integrator, n, dt):
     return np.asarray(qs)
 
 
+STARTQ = np.asarray([5.0, 5.0])
+
+GAUSSIAN_CENTERS = np.asarray([[0.0, 0.0], [4.0, 0.0]])
+GAUSSIAN_WEIGHTS = np.asarray([1.0, 10.0])
+
+# H = -log P(qcur) + p^2/2 (kinetic)
+def prob(qcur): 
+    s = 0
+    for i in range(GAUSSIAN_CENTERS.shape[0]):
+        c = GAUSSIAN_CENTERS[i]
+        w = GAUSSIAN_WEIGHTS[i]
+        s += w * np.exp(-1 * np.dot(qcur - c, qcur - c))
+    return s
+
+def dprob(qcur): 
+    s = 0
+    # d/dx(exp(- (x - μ)^2)) = (-2 (x - μ)) * exp(- (x - μ)^2)
+    for i in range(GAUSSIAN_CENTERS.shape[0]):
+        c = GAUSSIAN_CENTERS[i]
+        w = GAUSSIAN_WEIGHTS[i]
+        s += -2 * (qcur - c) * w * np.exp(-1 * np.dot(qcur - c, qcur - c))
+    return s
+
 # search for maxima of gaussian distribution
 def gaussian_hmc(startq, integrator, n, dt):
-    
-    # H = -log P(qcur) + p^2/2 (kinetic)
-    #e^{-(x^2)}
-    def logprob(qcur): return (-1 * np.dot(qcur, qcur))
-    def H(qcur, pcur): return - logprob(qcur)  + np.dot(pcur, pcur) / 2
+    def H(qcur, pcur): return -np.log(prob(qcur))  + np.dot(pcur, pcur) / 2
     def dhdp(qcur, pcur): return pcur
-    # - 1/P(qcur) . P'(qcur)
-    def dhdq(qcur, pcur): return -1.0/logprob(qcur) * (0.5 * qcur)
-    
+    def dhdq(qcur, pcur): return -1.0 / prob(qcur) * dprob(qcur)
 
     # starting positions
     qcur = startq.copy()
@@ -57,8 +74,10 @@ def gaussian_hmc(startq, integrator, n, dt):
 
         # redraw momentum
         qprop = qcur.copy()
-        pprop = np.array([np.random.normal(0, 2), np.random.normal(0, 2)])
-        for j in range(5):
+        pprop = np.array([np.random.normal(), np.random.normal()])
+        print ("initial proposal position: %s |  momentum:  %s" % (qprop, pprop))
+        N_PHYSICS_STEPS = 3
+        for _ in range(N_PHYSICS_STEPS):
             (qprop, pprop) = integrator(dhdp, dhdq, qprop, pprop, dt)
 
         # Negate momentum at end of trajectory to make the proposal symmetric
@@ -71,6 +90,8 @@ def gaussian_hmc(startq, integrator, n, dt):
         # minimise energy: new < old
         # [0-1] < old / new
         # log [0-1] < log old - log new
+        print("MH new position: %s | momentum: %s" % (qprop, pprop))
+
         if np.random.uniform() < np.exp(curH - propH):
             qcur = qprop.copy()
             pcur = pprop.copy()
@@ -91,7 +112,7 @@ def gaussian_mh(startq, n):
     naccept = 0
 
     for i in range(n):
-        qprop = qcur + 3 * np.array([np.random.normal(0, 2), np.random.normal(0, 2)])
+        qprop = qcur + np.array([np.random.normal(), np.random.normal()])
         # we want prob to increase
         if np.log(np.random.uniform()) < logprob(qprop) - logprob(qcur):
             qcur = qprop
@@ -100,9 +121,8 @@ def gaussian_mh(startq, n):
 
     return (naccept, np.asarray(qs))
 
-NITERS = 100
-TIMESTEP = 10
-STARTQ = np.asarray([1000.0, -1000.0])
+NITERS = 1000
+TIMESTEP = 0.1
 
 hmc_naccept, hmc_qs = gaussian_hmc(STARTQ, leapfroge, NITERS, TIMESTEP)
 
