22c4c2e5a778d1c10102f703e2b3fa31878f4930
Siddharth Bhat <siddu.druid@gmail.com>
2020-05-08 23:45:42 +0530

    don't build fiction

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -e
 
 make -C ~/blog/builder/build;
 LSAN_OPTIONS=detect_leaks=0 ./builder/build/builder ~/blog/README.md ~/blog/index.html --latex2ascii
-LSAN_OPTIONS=detect_leaks=0  ./builder/build/builder ~/blog/fiction.md ~/blog/fiction.html --latex2ascii
+# LSAN_OPTIONS=detect_leaks=0  ./builder/build/builder ~/blog/fiction.md ~/blog/fiction.html --latex2ascii
