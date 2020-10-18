07c39ef0286eba3d442ca7c55ef6a0c5946b01df
Siddharth Bhat <siddu.druid@gmail.com>
2020-10-18 14:47:21 +0530

    add slides

--- slides-why-haskell/subtle.cpp
+++ slides-why-haskell/subtle.cpp
@@ -2,11 +2,13 @@
 #include <climits>
 using namespace std;
 
-bool x_gt_x_plus_1(unsigned x) {
+bool f(unsigned x) {
     return (x + 1) > x;
 }
 
 int main() {
-    cout << x_gt_x_plus_1(0) << "\n";
-    cout << x_gt_x_plus_1(UINT32_MAX) << "\n";
+    cout << "f(0): " << f(0) << "\n";
+    cout << "f(UINT32_MAX):" << f(UINT32_MAX) << "\n";
+    cout << "UNIT32_MAX: " << UINT32_MAX << "\n";
+    cout << "1 + UINT32_MAX: " << 1 + UINT32_MAX << "\n";
 }
