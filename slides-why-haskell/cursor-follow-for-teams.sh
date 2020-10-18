dcb4e8299e8d8beabc91d2a01011a93ce58c9a8e
Siddharth Bhat <siddu.druid@gmail.com>
2020-10-18 20:07:11 +0530

    add the rest of slides

--- slides-why-haskell/cursor-follow-for-teams.sh
+++ slides-why-haskell/cursor-follow-for-teams.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+set -e
+set -o xtrace
 oneko -idle 0 -speed 100 -time 5120 -bg blue -fg orange -position +20+20
--- slides-why-haskell/fib-take-2.hs
+++ slides-why-haskell/fib-take-2.hs
@@ -1,3 +1,4 @@
 -- x[n+1] = x[n] + x[n-1]
 fibs = 0:1:(zipAdd fibs (tail fibs))
+
 zipAdd (x:xs) (y:ys) = (x+y):zipAdd xs ys
