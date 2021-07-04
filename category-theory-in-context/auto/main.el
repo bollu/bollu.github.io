da6c96e44202b9efd43c401d116cf9e0546fdeac
Siddharth Bhat <siddu.druid@gmail.com>
2021-07-04 18:35:45 +0530

    add 1.7

--- category-theory-in-context/auto/main.el
+++ category-theory-in-context/auto/main.el
@@ -2,24 +2,24 @@
  "main"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("book" "11pt")))
+                     '(("report" "14pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("mathpazo" "sc" "osf") ("eulervm" "euler-digits" "small") ("todonotes" "colorinlistoftodos" "prependcaption" "textsize=tiny")))
+                     '(("todonotes" "colorinlistoftodos" "prependcaption" "textsize=tiny")))
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
-    "book"
-    "bk11"
-    "mathpazo"
-    "eulervm"
+    "report"
+    "rep10"
+    "classicthesis"
+    "bbm"
     "bbding"
     "physics"
     "amsmath"
@@ -35,6 +35,7 @@
     "booktabs"
     "subcaption"
     "enumitem"
+    "mathtools"
     "xargs"
     "todonotes"
     "hyperref"
@@ -42,8 +43,13 @@
     "tabularx"
     "latexsym")
    (TeX-add-symbols
-    '("beginproof" ["argument"] 0)
+    '("proof" 1)
     '("question" 1)
+    '("start" 1)
+    '("inv" 1)
+    '("subcat" 2)
+    '("functor" 3)
+    '("cat" 1)
     '("pf" 1)
     '("pushfwd" 1)
     '("pullback" 1)
@@ -59,7 +65,26 @@
     "vectorfield"
     "boldX"
     "boldY"
+    "functordef"
+    "cc"
+    "ee"
+    "ccat"
+    "cset"
+    "opc"
+    "opd"
+    "ope"
+    "mono"
+    "epi"
+    "bg"
+    "bgg"
+    "nt"
+    "zero"
+    "one"
+    "two"
+    "three"
+    "id"
     "G"
+    "answer"
     "X"
     "comma"
     "qed")
