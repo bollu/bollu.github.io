2b1c37a67783585d30fc2f8fcb35998984a5934a
Siddharth Bhat <siddu.druid@gmail.com>
2021-12-31 12:57:03 +0000

    nix build set up

---
+++ builder/default.nix
@@ -0,0 +1,29 @@
+{ pkgs ? import <nixpkgs> {}, katexPath, prismPath, inputPath, outputPath}:
+
+with pkgs;
+
+
+stdenv.mkDerivation {
+
+	name = "builder";
+	src = ./.;
+	nativeBuildInputs = [ cmake ninja ];
+# 	buildPhase = ''
+# 		mkdir build; cd build; cmake -G Ninja ../.; ninja
+# 	'';
+# this combines configure and build into one phase, less than wholly cool
+
+	cmakeFlags = "-G Ninja";
+
+	postPatch = ''
+		set -e
+		sed -i --debug "s@const char CONFIG_KATEX_PATH.*@const char CONFIG_KATEX_PATH\[\]=\"${katexPath}\";@" builder.cpp
+		sed  -i --debug "s@const char CONFIG_PRISM_PATH.*@const char CONFIG_PRISM_PATH\[\]=\"${prismPath}\";@" builder.cpp
+		sed  -i --debug "s@const char CONFIG_OUTPUT_DIRECTORY_NO_TRAINING_SLASH.*@const char CONFIG_OUTPUT_DIRECTORY_NO_TRAINING_SLASH\[\]=\"${outputPath}\";@" builder.cpp
+		sed  -i --debug "s@const char CONFIG_INPUT_MARKDOWN_PATH.*@const char CONFIG_INPUT_MARKDOWN_PATH\[\]=\"${inputPath}\";@" builder.cpp
+	'';
+	buildPhase = ''ninja'';
+
+
+
+}
---
+++ default.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+with pkgs;
+
+stdenv.mkDerivation {
+	buildInputs = let
+		builder = pkgs.callPackage ./builder { 
+			katexPath = ./katex/katex.min.js; 
+			prismPath = ./prism/prism.js;
+			inputPath = ./README.md;
+			outputPath = ./.;
+		};
+	in [ builder ];
+	name = "bollu.github.io";
+	src = ./.;
+
+
+	buildPhase = "builder";
+}
