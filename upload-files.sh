1926d51890c94108ef6d6950720c1eeda1c4a3a5
Siddharth Bhat <siddu.druid@gmail.com>
2023-04-04 12:15:38 +0100

    add new file uploader into nix server.

--- upload-files.sh
+++ upload-files.sh
@@ -2,5 +2,11 @@
 set -o xtrace
 set -e
 
+make build
+
+git checkout github-pages
+git add -f out/
+git commit -m "github page upload"
+
 ssh root@pixel-druid.com "rm -rf /var/blog/"
 rsync -r --progress -avz ./ root@www.pixel-druid.com:/var/blog
