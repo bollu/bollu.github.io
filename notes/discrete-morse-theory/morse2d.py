5d19733a03f7ca899ba9b2e21565fb8f02f1818e
Siddharth Bhat <siddu.druid@gmail.com>
2024-03-09 18:00:19 +0000

    implement morse function and compute critical points

--- notes/discrete-morse-theory/morse2d.py
+++ notes/discrete-morse-theory/morse2d.py
@@ -132,27 +132,29 @@ class Chain:
         return self.val2coeff.items()
 
 
+def test_triangle():
+    a = ChElem("a", Chain())
+    b = ChElem("b", Chain())
+    c = ChElem("c", Chain())
 
-a = ChElem("a", Chain())
-b = ChElem("b", Chain())
-c = ChElem("c", Chain())
+    ab = ChElem("ab", b - a)
+    bc = ChElem("bc", c - b)
+    ca = ChElem("ca", a - c)
 
-ab = ChElem("ab", b - a)
-bc = ChElem("bc", c - b)
-ca = ChElem("ca", a - c)
+    abc = ChElem("abc", ab + bc + ca)
+    print("a,b,c:", a, b, c)
+    print("a+b+c:", a + b + c)
+    # print((ab + bc + ca) % 2)
 
-abc = ChElem("abc", ab + bc + ca)
-print("a,b,c:", a, b, c)
-print("a+b+c:", a + b + c)
-# print((ab + bc + ca) % 2)
+    # print(ab.boundary)  # boundary of vertex is 0
+    print("ab:", ab)
+    print("ab.boundary: ", ab.boundary)
 
-# print(ab.boundary)  # boundary of vertex is 0
-print("ab:", ab)
-print("ab.boundary: ", ab.boundary)
-
-print("abc:", abc)
-print("abc.boundary: ", abc.boundary)
-print("abc.boundary.boundary: ", abc.boundary.boundary)
+    print("abc:", abc)
+    print("abc.boundary: ", abc.boundary)
+    print("abc.boundary.boundary: ", abc.boundary.boundary)
+    return abc
+test_triangle()
 
 class Complex2:
     vertices: Set[Chain]
@@ -184,13 +186,129 @@ class Complex2:
         for vertex in self.vertices:
             assert isinstance(vertex, ChElem)
 
-        
+    def function(self) -> "Function":
+        return Function(self)
+
+    def get_elem_faces(self, elem):
+        assert elem in self
+        return elem.boundary
+
+    def get_elem_cofaces(self, elem):
+        assert elem in self
+        if elem in self.faces:
+            return set()
+        if elem in self.edges:
+            return set([face for face in self.faces if elem in face.boundary])
+        if elem in self.vertices:
+            return set([edge for edge in self.edges if elem in edge.boundary])
+        raise RuntimeError("unreachable!")
+
+    def dimension_function(self) -> "Function":
+        f = self.function()
+        for face in self.faces:
+            f[face] = 3
+        for edge in self.edges:
+            f[edge] = 2
+        for vertex in self.vertices:
+            f[vertex] = 1
+
+        return f
     def __str__(self):
         out = ""
         out += f"faces: {str(self.faces)}\n"
         out += f"edges: {set(self.edges)}\n"
         out += f"vertices: {str(self.vertices)}"
         return out
+    def contains(self, item):
+        return item in self.faces or \
+            item in self.edges or \
+            item in self.vertices
 
-print(Complex2(faces=[abc]))
+    def __iter__(self):
+        for vertex in self.vertices:
+            yield vertex
+        for edge in self.edges:
+            yield edge 
+        for face in self.faces:
+            yield face
 
+
+
+class Function:
+    simplex : Complex2
+    val : Dict[ChElem, int] # map each chain element to a value.
+    
+    def __str__(self):
+        out = []
+        for elem in self.simplex:
+            out.append(f"{elem}:{self[elem]}")
+        return " ".join(out)
+
+    def __init__(self, simplex):
+        self.simplex = simplex
+        self.val = dict()
+        assert isinstance(simplex, Complex2)
+
+
+    def set(self, elem, val):
+        assert elem in self.simplex
+        self.val[elem] = val
+    
+    def __getitem__(self, elem):
+        assert elem in self.simplex
+        if elem in self.val:
+            return self.val[elem]
+        else:
+            return 0
+
+    def __setitem__(self, elem, val):
+        self.set(elem, val)
+
+    def get_violations(self, elem):
+        """
+        returns lower dimensional faces where f(face) > f(elem),
+        and higher dimensional cofaces where f(coface) < f(elem)
+        """
+        lower_violations = set()
+        higher_violations = set()
+        for lower in self.simplex.get_elem_faces(elem):
+            if self[lower] > self[elem]:
+                lower_violations.add(lower)
+        for higher in self.simplex.get_elem_cofaces(elem):
+            if self[higher] < self[elem]:
+                higher_violations.add(higher)
+
+        return (lower_violations, higher_violations)
+
+    def is_critical_at(self, elem):
+        """A function is critical at a simplex if there is at most 1 lower violation
+        and at most 1 higher violation.
+        
+        The intuition is that a critical point, there is one direction in which the function
+        increases and one where it decreases, like a saddle.
+        """ 
+        (lower_violations, higher_violations) = self.get_violations(elem)
+        return len(lower_violations) <= 1 and len(higher_violations) <= 1
+
+    def critical_set(self):
+        """Set where the morse function is critical"""
+        out = set()
+        for elem in self.simplex:
+            if is_critical_at(elem):
+                out.add(elem)
+        return out
+    def is_morse(self):
+        for elem in self.simplex:
+            if not self.is_critical_at(elem):
+                return False
+        return True
+
+def test_complex_and_dimension():
+    abc = test_triangle()
+    abc_complex = Complex2(faces=[abc])
+    f = abc_complex.dimension_function()
+    print(f)
+    for elem in abc_complex:
+        print(f"violations for {elem}:{f.get_violations(elem)}")
+    print("dimension function is morse?: ", f.is_morse())
+test_complex_and_dimension()
