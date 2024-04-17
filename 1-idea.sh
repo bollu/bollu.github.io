b04cbc1ea9f68f9747fec9631cdcf018fe458d25
Siddharth Bhat <siddu.druid@gmail.com>
2024-04-17 10:40:53 +0100

    feat: tidy up

--- 1-idea.sh
+++
@@ -1,3 +0,0 @@
-# script to automatically tie `i` to the command to log ideas
-alias idea="vim ~/blog/README.md && (pushd ~/blog; git add README.md; git commit; git push origin master; popd)"
-alias i=idea
